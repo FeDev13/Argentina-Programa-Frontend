5cd4efaf4b7beae43f82b2e21a8658aa22763d2f
unknown <federicorobledo85@gmail.com>
2022-07-10 01:59:05 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { AcademicaComponent } from './components/academica/academica.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { HttpClientModule } from "@angular/common/http";
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    RedesComponent,
+    BannerComponent,
+    AboutComponent,
+    ExperienciaComponent,
+    AcademicaComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    HomeComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
