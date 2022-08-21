6cf82d62b0b331bc18aacbcec393bb461c9aac9d
unknown <federicorobledo85@gmail.com>
2022-08-20 23:27:01 -0300

    correccion academica

--- src/app/components/academica/academica.component.ts
+++ src/app/components/academica/academica.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Educacion } from 'src/app/model/educacion';
-import { EducacionService } from 'src/app/service/educacion.service';
 import { TokenService } from 'src/app/service/token.service';
 
 @Component({
@@ -10,38 +9,10 @@ import { TokenService } from 'src/app/service/token.service';
 })
 export class AcademicaComponent implements OnInit {
 
-  educacion: Educacion[] = [];
+ constructor(){}
 
-  constructor(private educacionS: EducacionService, private tokenService: TokenService) { }
-  isLogged = false;
-
-  ngOnInit(): void {
-    this.cargarEducacion();
-    if(this.tokenService.getToken()){
-      this.isLogged = true;
-    } else {
-      this.isLogged = false;
-    }
-  }
-
-  cargarEducacion(): void{
-    this.educacionS.lista().subscribe(
-      data =>{
-        this.educacion = data;
-      }
-    )
-  }
-
-  delete(id?: number){
-    if( id != undefined){
-      this.educacionS.delete(id).subscribe(
-        data => {
-          this.cargarEducacion();
-        }, err => {
-          alert("No se pudo eliminar");
-        }
-      )
-    }
-  }
+ ngOnInit(): void {
+   
+ }
 
 }
